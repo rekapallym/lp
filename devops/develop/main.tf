d1cb55f893741fa2badab2eeadb0b7203aad82db
ananddaparthi <anand.daparthi1@gmail.com>
2020-08-20 23:06:34 -0700

    more fix

--- devops/develop/main.tf
+++ devops/develop/main.tf
@@ -1,8 +1,8 @@
 module "lpfeed" {
     source = "../modules/lpfeed/"
-    image = var.image_name
-    replica_count = var.replicas
-    service_type = var.servicetype
+    image = var.image
+    replica_count = var.replica_count
+    service_type = var.service_type
     containerport = var.containerport
     targetport = var.targetport
     namespace = var.namespace
