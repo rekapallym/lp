89b848737c3de4864dbfa2ddb6f8216852464233
rekapallym <rekapallym@icloud.com>
2020-11-18 23:14:13 -0800

    first phase-courses

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {Options} from '../../models/OptionsModel';
+import { v4 as uuid } from 'uuid';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-courses',
@@ -9,14 +15,16 @@ import {Options} from '../../models/OptionsModel';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore, 
+    private toastr: ToastrService, 
+    private storage: AngularFireStorage,) { }
   
-  levels: Options[] = [{id: 1, value:'one'},{id:2, value:'two'},{id:3, value:'three'},
-  {id:4, value:'four'},{id:5, value:'five'},{id:6, value:'six'},{id:7, value:'seven'},
-  {id:8, value:'eight'},{id:9, value:'nine'},{id:10, value:'ten'}];
+  // levels: Options[] = [{id: 1, value:'one'},{id:2, value:'two'},{id:3, value:'three'},
+  // {id:4, value:'four'},{id:5, value:'five'},{id:6, value:'six'},{id:7, value:'seven'},
+  // {id:8, value:'eight'},{id:9, value:'nine'},{id:10, value:'ten'}];
 
 //todo chnage detection for the loop
-
+  seletedImage = null;
   courseForm = new FormGroup({
     category: new FormControl(),
     topicName: new FormControl(),
@@ -25,18 +33,59 @@ export class CoursesComponent implements OnInit {
     price: new FormControl(),
     discountPercentage: new FormControl('%'),
     title: new FormControl(),
-    difficulty_level: new FormControl()
-  })
+    difficulty_level: new FormControl(),
+    imageUrl : new FormControl()
+  });
+  
+  date = new Date();
 
   ngOnInit(): void {
   }
 
-  courseSubmit(){
-    console.log(this.courseForm.value);
+  courseSubmit(formValue){
+
+    if(this.seletedImage != null){
+      let filePath =  `courseImages/${this.seletedImage.name}_${this.date.getTime()}`;
+      const fileRef = this.storage.ref(filePath);
+  
+      this.storage.upload(filePath, this.seletedImage).snapshotChanges().pipe(
+        finalize(() => {
+          fileRef.getDownloadURL().subscribe((url) => {
+            formValue['imageUrl'] = url;
+            this.firestore.collection('content').doc("course").collection('course').add(coursesObject);
+          });
+        })
+      ).subscribe();
+      const coursesObject = {
+        id: uuid(),
+        title: 'courses',
+        description: this.courseForm.value,
+        metadata: {
+          date: this.date.toDateString(),
+          time: this.date.getTime().toString(),
+        },
+      };
+      console.log(coursesObject);
+  
+    }else{
+      const coursesObject = {
+        id: uuid(),
+        title: 'courses',
+        description: this.courseForm.value,
+        metadata: {
+          date: this.date.toDateString(),
+          time: this.date.getTime().toString(),
+        },
+      };
   }
+}
 
   get f(){
     return this.courseForm.controls; 
+  }
+
+  onFileSelected(event) {
+    this.seletedImage = event.target.files[0];
   }
 
   esetForm() {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,27 +1,27 @@
-export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDmlo_4Bv_RPLbytumUPDzNokGuD4P46vY",
-    authDomain: "capital-group-infra.firebaseapp.com",
-    databaseURL: "https://capital-group-infra.firebaseio.com",
-    projectId: "capital-group-infra",
-    storageBucket: "capital-group-infra.appspot.com",
-    messagingSenderId: "209798498337",
-    appId: "1:209798498337:web:22feab31141881765abb60",
-    measurementId: "G-M9KQVZSEJM"
-  }
-};
-
 // export const environment = {
-//   production: false,
+//   production: true,
 //   firebaseConfig: {
-//     apiKey: "AIzaSyBkGN9JbyQP6cYUQRO4WE8Yf07NYlTrnDA",
-//     authDomain: "learningpad-e5847.firebaseapp.com",
-//     databaseURL: "https://learningpad-e5847.firebaseio.com",
-//     projectId: "learningpad-e5847",
-//     storageBucket: "learningpad-e5847.appspot.com",
-//     messagingSenderId: "895961943679",
-//     appId: "1:895961943679:web:dc6c0cd8f2afb2f9ce794a",
-//     measurementId: "G-YQWB9KVJ99"
+//     apiKey: "AIzaSyDmlo_4Bv_RPLbytumUPDzNokGuD4P46vY",
+//     authDomain: "capital-group-infra.firebaseapp.com",
+//     databaseURL: "https://capital-group-infra.firebaseio.com",
+//     projectId: "capital-group-infra",
+//     storageBucket: "capital-group-infra.appspot.com",
+//     messagingSenderId: "209798498337",
+//     appId: "1:209798498337:web:22feab31141881765abb60",
+//     measurementId: "G-M9KQVZSEJM"
 //   }
 // };
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBkGN9JbyQP6cYUQRO4WE8Yf07NYlTrnDA",
+    authDomain: "learningpad-e5847.firebaseapp.com",
+    databaseURL: "https://learningpad-e5847.firebaseio.com",
+    projectId: "learningpad-e5847",
+    storageBucket: "learningpad-e5847.appspot.com",
+    messagingSenderId: "895961943679",
+    appId: "1:895961943679:web:dc6c0cd8f2afb2f9ce794a",
+    measurementId: "G-YQWB9KVJ99"
+  }
+};
